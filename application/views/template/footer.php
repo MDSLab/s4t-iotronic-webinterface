44a46c9b867fadb0be358d1dbdf26c6d42e5c6ba
caromeo <carmelo.romeo85@gmail.com>
2016-01-29 13:48:03 +0100

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,6 +1,5 @@
 		<div id="footer" align="center" style="z-index: 999;">
 			<p>Powered by MDSLab <br />Visit: <a href="http://smartme.unime.it/it/">SmartME</a> and <a href="http://stack4things.unime.it/">Stack4Things</a></p>
-		<p>An absolute URL</p>
                 </div>
 
 		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/vendor/jquery.js"></script>
