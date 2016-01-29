5e21a3a9e88607742f4ec91ac126891c305650f5
caromeo <carmelo.romeo85@gmail.com>
2016-01-29 11:05:36 +0100

    Create footer.php

---
+++ application/views/template/footer.php
@@ -0,0 +1,13 @@
+		<div id="footer" align="center" style="z-index: 999;">
+			<p>Powered by MDSLab <br />Visit: <a href="http://smartme.unime.it/it/">SmartME</a> and <a href="http://stack4things.unime.it/">Stack4Things</a></p>
+		<p>An absolute URL</p>
+                </div>
+
+		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/vendor/jquery.js"></script>
+		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation.min.js"></script>
+		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation/foundation.reveal.js"></script>
+		<script>  $(document).foundation();  </script>
+		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation/foundation.js"></script>
+
+	</body>
+</html>
