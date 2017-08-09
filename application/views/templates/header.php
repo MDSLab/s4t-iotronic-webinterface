9041422a464e4049c13cce2723c638133278cbb0
Giandomenico La Fauci <giandolf@users.noreply.github.com>
2017-08-09 22:39:18 +0200

    Restyling

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>S4T WebPage</title>
-		<link rel="stylesheet" href="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/css/foundation.min.css" />
-		<link rel="stylesheet" href="<?= $this -> config -> site_url() ?>assets/smartme.css" />
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/vendor/modernizr.js"></script>
-	</head>
-	<body>
 
-		<div id="header">
-			<table border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse; background-color:rgba(0, 0, 0, 0);" >
-				<tr>
-			<td>
-			<img src="<?= $this -> config -> site_url() .'uploads/mdslab.png'  ?>" height="90" width="170" />
-			</td>
-			<td valign="bottom">
-			<h1>Stack4Things</h1>
-			</td>
-				</tr>
-			</table>
-		</div>
+<head>
+    <title>S4T WebPage</title>
+    <link rel="stylesheet" href="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/css/foundation.min.css" />
+    <link rel="stylesheet" href="<?= $this -> config -> site_url() ?>assets/smartme.css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/vendor/modernizr.js"></script>
+    <!-- Openstreetmap -->
+    <!-- Leaflet -->
+    <link rel="stylesheet" type="text/css" href="<?= $this -> config -> site_url() ?>assets/leaflet.css" />
+    <link rel="stylesheet" type="text/css" href="<?= $this -> config -> site_url() ?>assets/MarkerCluster.Default.css" />
+    <link rel="stylesheet" type="text/css" href="<?= $this -> config -> site_url() ?>assets/MarkerCluster.css" />
+    <script src="<?= $this -> config -> site_url() ?>assets/leaflet.js"></script>
+    <script src="<?= $this -> config -> site_url() ?>assets/ll_MarkerCluster.js"></script>
+    <!-- OpenLayers -->
+    <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+    <div id="header">
+        Stack4Things
+    </div>
